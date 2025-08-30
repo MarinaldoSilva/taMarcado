6bbf5c4ae4b838ea4e2e3c1acebd076bda391f4d
Mario <marinaldo12@hotmail.com>
2025-08-29 21:47:08 -0300

    classe AgendamentoViewDetail criada para herdar a APIView, para modernizar o sistema de endpoints

--- agenda/views.py
+++ agenda/views.py
@@ -4,27 +4,25 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpRequest
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-def agendamento_detail_2(request : HttpRequest, id):
-    agendamento = get_object_or_404(Agendamento, id=id)
+class AgendamentoDetailView(APIView):
 
+    def agendamento_detail(request : HttpRequest, id_param):
+        try:
+            agendamento = Agendamento.objects.get(id=id_param)
+            serializer = AgendamentoSerializer(agendamento)
 
-def agendamento_detail(request : HttpRequest, id_param):
-    try:
-        agendamento = Agendamento.objects.get(id=id_param)
-        serializer = AgendamentoSerializer(agendamento)
-
-        return Response(
-            {'agendamento': serializer.data},
-            status=status.HTTP_200_OK
-        )
-    
-    except ObjectDoesNotExist as e:
-        return Response(
-            {"detail": "Agendamento não encontrado"},
-            status=status.HTTP_404_NOT_FOUND
-        )
+            return Response(
+                {'agendamento': serializer.data},
+                status=status.HTTP_200_OK
+            )
+        
+        except ObjectDoesNotExist as e:
+            return Response(
+                {"detail": "Agendamento não encontrado"},
+                status=status.HTTP_404_NOT_FOUND
+            )
         
         
     
