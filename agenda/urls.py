163fd751c7fd20df296e2ae6c6dbbfd4e3d536bf
Mario <marinaldo12@hotmail.com>
2025-09-01 15:37:17 -0300

    alteração das rotas para que as funções que foram criadas manulamente sejam executadas

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from agenda.Views.views2 import agendamentoDetail, agendamentoList
 from agenda.Views.views import AgendametoListCreateView, AgendamentoRetriveUpdateDestroyView
 
 urlpatterns = [
     path("agendamentos/", AgendametoListCreateView.as_view(), name= "agendamento-list"),
-    path("agendamentos/<int:id>", AgendamentoRetriveUpdateDestroyView.as_view(), name="agendamento-detail")
-    
+    path("agendamentos/<int:pk>", AgendamentoRetriveUpdateDestroyView.as_view(), name="agendamento-detail"),
+
+    path("agendamentos_n/<int:pk>/", agendamentoDetail, name="agendamento-detail"),
+    path("agendamentos_n/", agendamentoList, name="agendamento_list")
 ]
+
