9043ffdaa4173d8bb84729a36c53263d9e96d694
Mario <marinaldo12@hotmail.com>
2025-09-02 14:41:07 -0300

    correção da rota com barra extra

--- agenda/urls.py
+++ agenda/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("agendamentos/", AgendametoListCreateView.as_view(), name= "agendamento-list"),
     path("agendamentos/<int:pk>", AgendamentoRetriveUpdateDestroyView.as_view(), name="agendamento-detail"),
 
-    path("agendamentos_n/<int:pk>/", agendamentoDetail, name="agendamento-detail"),
-    path("agendamentos_n/", agendamentoList, name="agendamento_list")
+    path("agendamentos_n/<int:pk>", agendamentoDetail, name="agendamento-detail"),
+    path("agendamentos_n", agendamentoList, name="agendamento_list")
 ]
 
