f4bcc203a3c6a5bfba44a9d8dd7151675186aa44
mario <marinaldo12@hotmail.com>
2025-09-02 19:07:15 -0300

    update gerador de chave

--- secret_key.py
+++ secret_key.py
@@ -1,2 +1,7 @@
 from django.core.management.utils import get_random_secret_key
+
 print(get_random_secret_key())
+
+
+"""random secret key, gera uma chave nova para ser adicionado no nosso .env
+cria uma variável com o nome SECRET_KEY='sua chave' e rode o migrate para subir o banco"""
